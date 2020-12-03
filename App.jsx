c79dbdea0c48c05d3bac80db22763b12f98deff2
chavezbryanr <chavezbryanr@gmail.com>
2020-12-02 21:21:47 -0600

    Added header

--- App.jsx
+++ App.jsx
@@ -1,10 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Header } from 'react-native-elements';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Header
+          leftComponent={{ icon: 'menu', color: '#fff', onPress: () => alert('Menu Pressed!'), }}
+          centerComponent={{ text: 'Instant Invoice', style: { color: '#fff', fontSize: 25 } }}
+          rightComponent={{ icon: 'home', color: '#fff', onPress: () => alert('Home Pressed!') }}
+          backgroundColor="#0db512"
+        />
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
@@ -16,6 +23,5 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
   },
 });
